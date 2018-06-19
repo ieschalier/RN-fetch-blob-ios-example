9749017bb2e135ff4d2c0da4848761425251565a
Igor ESCHALIER <igor.eschalier@gmail.com>
2018-06-19 16:59:21 +0200

    Link react-native-pdf to ios project

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, Dimensions, View } from 'react-native'
+
 import FetchBlob from 'react-native-fetch-blob'
+import Pdf from 'react-native-pdf'
 
 export default class App extends React.Component {
   render() {
+    const source = {
+      uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf',
+      cache: true,
+    }
+
     return (
       <View style={styles.container}>
         <Text>{FetchBlob.fs.dirs.DocumentDir}</Text>
+        <Pdf
+          source={source}
+          onLoadComplete={(numberOfPages, filePath) => {
+            console.log(`number of pages: ${numberOfPages}`)
+          }}
+          onPageChanged={(page, numberOfPages) => {
+            console.log(`current page: ${page}`)
+          }}
+          onError={error => {
+            console.log(error)
+          }}
+          style={{
+            flex: 1,
+            width: Dimensions.get('window').width,
+          }}
+        />
       </View>
     )
   }
